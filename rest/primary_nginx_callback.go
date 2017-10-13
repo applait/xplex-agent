106e777273bc0daa365c81c7eb31080b8a344d86
Jai <jai@applait.com>
2017-10-13 16:16:05 +0530

    Adds execworker, initiates cron and removes worker mode
    Signed-off-by: Jai <jai@applait.com>

--- rest/primary_nginx_callback.go
+++ rest/primary_nginx_callback.go
@@ -3,9 +3,12 @@ package rest
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/applait/xplex-agent/execworker"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 	"github.com/spf13/viper"
@@ -21,7 +24,7 @@ type rtmpConnectReq struct {
 	PageURL  string `schema:"pageUrl"`
 }
 
-type rtmpPlayReq struct {
+type rtmpPublishReq struct {
 	Call     string `schema:"call"`
 	Addr     string `schema:"addr"`
 	ClientID string `schema:"clientid"`
@@ -33,7 +36,7 @@ type rtmpPlayReq struct {
 	Name     string `schema:"name"`
 }
 
-func proxyReq(req interface{}, w http.ResponseWriter) {
+func proxyReq(req interface{}) (*http.Response, error) {
 	payload, err := json.Marshal(req)
 	if err != nil {
 		log.Println(err)
@@ -45,11 +48,11 @@ func proxyReq(req interface{}, w http.ResponseWriter) {
 	client := &http.Client{}
 	proxyResp, err := client.Do(proxyReq)
 	if err != nil {
-		panic(err)
+		return proxyResp, err
 	}
 	defer proxyReq.Body.Close()
 
-	w.WriteHeader(proxyResp.StatusCode)
+	return proxyResp, nil
 }
 
 func rtmpConnect(w http.ResponseWriter, r *http.Request) {
@@ -66,11 +69,17 @@ func rtmpConnect(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	proxyReq(req, w)
+	proxyResp, err := proxyReq(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Proxy back status code to primary nginx
+	w.WriteHeader(proxyResp.StatusCode)
 }
 
-func rtmpPlay(w http.ResponseWriter, r *http.Request) {
-	req := new(rtmpPlayReq)
+func rtmpPublish(w http.ResponseWriter, r *http.Request) {
+	req := new(rtmpPublishReq)
 	err := r.ParseForm()
 	if err != nil {
 		log.Println(err)
@@ -83,14 +92,31 @@ func rtmpPlay(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	proxyReq(req, w)
+	proxyResp, err := proxyReq(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Store stream specific secondary nginx configuration
+	out, err := os.Create(viper.GetString("nginx.streamConfigBasePath") + req.Name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	io.Copy(out, proxyResp.Body)
+
+	// Spin secondary nginx process
+	execworker.SpinNginx(req.Name)
+
+	// Proxy back status code to primary nginx
+	w.WriteHeader(proxyResp.StatusCode)
 }
 
 func rtmpHandler(r *mux.Router) {
 	rpost := r.Methods("POST").Subrouter()
 
 	rpost.HandleFunc("/on_connect", rtmpConnect)
-	rpost.HandleFunc("/on_play", rtmpPlay)
+	rpost.HandleFunc("/on_publish", rtmpPublish)
 }
 
 func callbackHandler(r *mux.Router) {
