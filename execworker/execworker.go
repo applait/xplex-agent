186056c4f2a16a2aea11bd936bda98f2ad9ee361
Jai <jai@applait.com>
2018-03-11 02:27:13 +0530

    Adds nginx block updater and stream manager
    Signed-off-by: Jai <jai@applait.com>

--- execworker/execworker.go
+++ execworker/execworker.go
@@ -1,14 +1,43 @@
 package execworker
 
 import (
+	"errors"
 	"io/ioutil"
 	"os/exec"
 	"strconv"
 	"syscall"
 )
 
-// StartNginx starts secondary nginx process for specific stream
-func StartNginx(configPath string, pidPath string) error {
+func StartStreamer(streamer string, configPath string, pidPath string) error {
+	switch streamer {
+	case "nginx":
+		err := startNginx(configPath, pidPath)
+		if err != nil {
+			return nil
+		}
+	default:
+		return errors.New("Unknown streamer")
+	}
+
+	return nil
+}
+
+func StopStreamer(streamer string, pidPath string) error {
+	switch streamer {
+	case "nginx":
+		err := stopNginx(pidPath)
+		if err != nil {
+			return nil
+		}
+	default:
+		return errors.New("Unknown streamer")
+	}
+
+	return nil
+}
+
+// startNginx starts secondary nginx process for specific stream
+func startNginx(configPath string, pidPath string) error {
 	err := exec.Command("nginx", "-c", configPath, "-g", "'pid "+pidPath+";'").Run()
 	if err != nil {
 		return err
@@ -17,8 +46,8 @@ func StartNginx(configPath string, pidPath string) error {
 	return nil
 }
 
-// StopNginx accepts a pidPath and kills the process
-func StopNginx(pidPath string) error {
+// stopNginx accepts a pidPath and kills the process
+func stopNginx(pidPath string) error {
 	out, err := ioutil.ReadFile(pidPath)
 	if err != nil {
 		return err
